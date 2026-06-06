53c9f586bbe4ea187a275fbe8a474951cda13c9c
DizzSilver <74016239+DizzSilver@users.noreply.github.com>
2026-06-06 16:11:26 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Il codice sorgente è rilasciato sotto licenza MIT.
+
+Tutti i diritti relativi al logo, all’identità visiva, alle immagini e ai contenuti testuali sono riservati.
+
+© 2024-2026 Dennis Benco
---
+++ js/toggleTheme.js
@@ -0,0 +1,39 @@
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+function updateButton({ buttonEl, isDark, nav, footer }) {
+  const newCta = isDark ? "url('/images/theme-dark.svg')" : "url('/images/theme-light.svg')";
+  buttonEl.style.backgroundImage = newCta;
+}
+
+function updateThemeOnHtmlEl({ theme }) {
+  document.querySelector("html").setAttribute("data-theme", theme);
+}
+
+const button = document.getElementById("data-theme-toggle");
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
+updateThemeOnHtmlEl({ theme: currentThemeSetting });
+
+button.addEventListener("click", (event) => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  localStorage.setItem("theme", newTheme);
+  updateButton({ buttonEl: button, isDark: newTheme === "dark" });
+  updateThemeOnHtmlEl({ theme: newTheme });
+
+  currentThemeSetting = newTheme;
+}); 
